103045f7170780d792564ab43d85c6331e38a02f
Tas Yusoontorn <mac@Macs-MacBook-Air-8.local>
2017-08-10 16:16:40 +0700

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat dinner'}).then((result) => {
+  //   console.log(result);
+  // });
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Tas'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').deleteOne({
+    _id: new ObjectID('598be48378d2f63959b5eb99')
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
